24ca01e1271982f4472bb77918d3d4e6b8cebbe2
Chunhui Gu <chunhui.gu@alumni.emory.edu>
2023-03-08 12:06:17 -0600

    fix bug loss different devices

--- graphgym/custom_graphgym/loss/protein.py
+++ graphgym/custom_graphgym/loss/protein.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 def loss_protein(pred, true):
     if cfg.model.loss_fun == 'binary_cross_entropy_with_weight':
         # need to implement loss accepting customized weight
-        pos_weight = torch.tensor([cfg.model.loss_pos_weight], dtype=torch.float)
+        pos_weight = torch.tensor([cfg.model.loss_pos_weight], dtype=torch.float).to(cfg.accelerator)
         bce_loss = torch.nn.BCEWithLogitsLoss(reduction='mean', pos_weight=pos_weight)
         true = true.float()
         loss = bce_loss(pred, true)
