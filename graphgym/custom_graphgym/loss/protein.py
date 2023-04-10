e34c15e03fe73058a2c3e0c05bbb3152990e4258
Chunhui Gu <chunhui.gu@alumni.emory.edu>
2023-04-10 17:42:33 -0500

    add functionality for load model and eval model

--- graphgym/custom_graphgym/loss/protein.py
+++ graphgym/custom_graphgym/loss/protein.py
@@ -10,16 +10,15 @@ import torch.nn as nn
 def loss_protein(pred, true):
     if cfg.model.loss_fun == 'binary_cross_entropy_with_weight':
         # automatically calculate weight based on proportion of postivie/negative labels
-        if cfg.model.loss_pos_weight == -1:
-            y_np = true.detach().cpu().numpy()
-            pos_weight = class_weight.compute_class_weight('balanced', classes=np.unique(y_np), y=y_np)
-            pos_weight = pos_weight[1]/pos_weight[0]
-            pos_weight = torch.tensor([pos_weight], dtype=torch.float).to(cfg.accelerator)
-        else:
-            pos_weight = torch.tensor([cfg.model.loss_pos_weight], dtype=torch.float).to(cfg.accelerator)
+        pos_weight = torch.tensor([cfg.model.loss_pos_weight], dtype=torch.float).to(cfg.accelerator)
         bce_loss = torch.nn.BCEWithLogitsLoss(reduction='mean', pos_weight=pos_weight)
         true = true.float()
         loss = bce_loss(pred, true)
         return loss, torch.sigmoid(pred)
 
-
+        # if cfg.model.loss_pos_weight == -1:
+        #     y_np = true.detach().cpu().numpy()
+        #     pos_weight = class_weight.compute_class_weight('balanced', classes=np.unique(y_np), y=y_np)
+        #     pos_weight = pos_weight[1]/pos_weight[0]
+        #     pos_weight = torch.tensor([pos_weight], dtype=torch.float).to(cfg.accelerator)
+        # else:
